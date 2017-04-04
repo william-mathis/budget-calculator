4e84ba5092f72ce69568c8c4c1384072f3e34aae
Will Mathis <wmathis@Wills-MacBook-Pro.local>
2017-04-04 18:49:24 -0400

    TC fixed everything

--- main.js
+++ main.js
@@ -1,32 +1,34 @@
-$(document).ready(function () {
-	$("#calculate-button").on("click", function (e) {
-		e.preventDefault();
-		console.log("click worked")
-		if (document.getElementById("#waste-water-loan").checked) {
-			$("#big-number").val(agPlus)
-		} else {
-			$("#big-number").val(bigNumber)
-			$("#waste-water-loan").css("text-decoration", "line-through")
-		};
-		$("#big-number").val(bigNumber);
-	});
-	var agPlus = agriculture + bigNumber;
-});
+// $(document).ready(function () {
+// 	$("#calculate-button").on("click", function (e) {
+// 		e.preventDefault();
+// 		console.log("click worked")
+// 		if (document.getElementById("#waste-water-loan").checked) {
+// 			$("#big-number").val(agPlus)
+// 		} else {
+// 			$("#big-number").val(bigNumber)
+// 			$("#waste-water-loan").css("text-decoration", "line-through")
+// 		};
+// 		$("#big-number").val(bigNumber);
+// 	});
+// 	var agPlus = agriculture + bigNumber;
+// });
+//
+// function myFunction() {
+// 	var x = document.getElementById("myCheckbox");
+// 	x.checked = true;
+// }
+//
+//
+// var bigNumber = 54000000000 //total defense spending increase//
+//
+// var wastewater = 498000000
+//
+// function agriculture(a, b, c, d) {
+// 	var ag = 498000000 + 95000000 + 195000000 + 3912000000;
+// 	return ag;
+// }
 
-function myFunction() {
-	var x = document.getElementById("myCheckbox");
-	x.checked = true;
-}
-
-
-var bigNumber = 54000000000 //total defense spending increase//
-
-var wastewater = 498000000
-
-function agriculture(a, b, c, d) {
-	var ag = 498000000 + 95000000 + 195000000 + 3912000000;
-	return ag;
-}
+var totalBudget;
 
 $(document).ready(function () {
 	var $window = $(window),
@@ -36,6 +38,24 @@ $(document).ready(function () {
 	$window.scroll(function () {
 		$stickyEl.toggleClass('sticky', $window.scrollTop() > elTop);
 
+	});
+});
+
+$(document).ready(function () {
+	totalBudget = parseInt($(".defense").text());
+
+	$("input[type='checkbox']").on("click", function (e) {
+		var total = 0;
+		var newBudget = totalBudget;
+
+		$("input[type='checkbox']:checked").each(function () {
+			var checkbox = $(this);
+			var value = parseInt(checkbox.data("cut"));
+			total = total + value;
+		});
+
+		newBudget = newBudget - total;
+		$(".defense").text(newBudget);
 	});
 });
 
