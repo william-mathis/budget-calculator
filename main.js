b00a3e09ad6d48dd7f7d449b45ad8f2ef5cdd575
Will Mathis <wmathis@Wills-MacBook-Pro.local>
2017-04-04 14:53:45 -0400

    sticky box

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
 			$("#big-number").val(agPlus)
 		} else {
 			$("#big-number").val(bigNumber)
-
+			$("#waste-water-loan").css("text-decoration", "line-through")
 		};
 		$("#big-number").val(bigNumber);
 	});
@@ -27,6 +27,17 @@ function agriculture(a, b, c, d) {
 	var ag = 498000000 + 95000000 + 195000000 + 3912000000;
 	return ag;
 }
+
+$(document).ready(function () {
+	var $window = $(window),
+		$stickyEl = $('#big-number'),
+		elTop = $stickyEl.offset().top;
+
+	$window.scroll(function () {
+		$stickyEl.toggleClass('sticky', $window.scrollTop() > elTop);
+
+	});
+});
 
 
 //When you click a checkbox you need it to find that amount and then subtract it from the total. //
